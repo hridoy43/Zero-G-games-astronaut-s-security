2ae3c6114d51354df2b6ee4d71d4c48c5bc37dc3
Wahiduzzaman Hridoy <hridoy43@gmail.com>
2016-04-22 22:24:33 -0700

    Update Zero-G_Drill_Game_and_Astronaut_Security.ino

--- Zero-G_Drill_Game_and_Astronaut_Security.ino
+++ Zero-G_Drill_Game_and_Astronaut_Security.ino
@@ -5,20 +5,38 @@
 int led = 13; //led light
 int Alarm = 12; //buzzer pin
 
+//Laser box Ray1 (Motion)
+int pirPin1 = 6;
+int pirSupply1 = 7;
+
+//Laser box Ray2 (Motion)
+int pirPin2 = 8;
+int pirSupply2 = 9;
+
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
   pinMode(led,LOW);
   pinMode(Alarm,LOW);
   
+  pinMode(pirPin1, INPUT);
+  pinMode(pirSupply1,OUTPUT);
+  
+  pinMode(pirPin2, INPUT);
+  pinMode(pirSupply2,OUTPUT);
+  
   Serial.begin(9600); // Open serial monitor at 115200 baud to see ping results.
 }
 
 void loop() {
+     digitalWrite(pirSupply1,HIGH);
+     digitalWrite(pirSupply2,HIGH);
+  
   delay(50);                          // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
   unsigned int uS = sonar.ping_cm(); // Send ping, get ping time in centemeter (uS).
   delay(100);
-  if(uS >= 50){
+  
+  if(uS >= 35){          //For Outside Game
     pinMode(led,HIGH);
     pinMode(Alarm,HIGH);
     Serial.print("Artronaut is drifting away from the sequred area");
@@ -28,5 +46,17 @@ void loop() {
     pinMode(led,LOW);
     pinMode(Alarm,LOW);
   }
+  
+  delay(100);
+  
+  if ((digitalRead(pirPin1) == HIGH) || (digitalRead(pirPin2) == HIGH))  ////For Inside laser box Game
+    {
+     
+     Serial.print("Object detected in laser box");
+     Serial.println(" ");
+     digitalWrite(pirSupply1,LOW);
+     digitalWrite(pirSupply2,LOW);
+    }
+  
   delay(1000);
 } 
